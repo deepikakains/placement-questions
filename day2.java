c6bac9bd0bbc885d37f848aabcfa28b3fc47bd91
Deepika Kains <125765903+deepikakains@users.noreply.github.com>
2023-09-04 14:21:42 +0530

    Add files via upload

---
+++ day2.java
@@ -0,0 +1,19 @@
+
+1.(E) Mother in law
+2.(c) 1941
+3.(b) Dear Mr.Smith;
+4.
+import java .util.*;
+public class day2{
+    public static void main(String args[]){
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter height of a cone");
+        float h=sc.nextFloat();
+        System.out.println("Enter radius of cone");
+        float r=sc.nextFloat();
+        double volume =3.14*r*r*h/3;
+        System.out.println("Volume of a cone is :" + volume);
+    }
+}
+
+5.(b)11
