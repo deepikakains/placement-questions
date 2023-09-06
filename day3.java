5020308e32616dbf73edaac3bf0426b025e1fb13
Deepika Kains <125765903+deepikakains@users.noreply.github.com>
2023-09-06 20:07:56 +0530

    Add files via upload

---
+++ day3.java
@@ -0,0 +1,16 @@
+1.(a)23% profit
+2.(c)Printer Explaination -> Printer is odd one because here keyboard,mouse and scanner are computer input device where as printer is a computer output device.
+3.(d)inevitable,standards,imperatives
+4.
+public class day3{
+    public static void main(String args[]){
+        for(int i=0;i<4;i++){
+            for(int j=0;j<8;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
+5.(d)3,7
+
