a783919893cea3d8eadada37f1d591c92ab3d49c
Deepika Kains <125765903+deepikakains@users.noreply.github.com>
2023-09-10 11:27:43 +0530

    Add files via upload

---
+++ day4.java
@@ -0,0 +1,14 @@
+(1).(a)70days
+(2).i-(c)Rambling
+    ii-(a)Misgiving
+(3).(b)8.224
+(4).(d)i=i+5
+    integer i=0
+    integer sum=0
+    while(i<=50){
+        sum=sum+i
+        i=i+5
+    }
+    print sum
+(5).(d)Null operator
+(6).(d)array    
