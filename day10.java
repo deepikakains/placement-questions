c307badc29c5f70845c2fc7fcd3acf2678799c20
Deepika Kains <125765903+deepikakains@users.noreply.github.com>
2023-12-10 23:51:00 +0530

    Add files via upload

---
+++ day10.java
@@ -0,0 +1,15 @@
+Q1.What are the different cloud computing service models?
+ANS->1.Infrastructure as a service(IaaS)
+     2.Platform as a service(PaaS)
+     3.Software as a service(SaaS)
+Q2.A basket contains 3 blue, 5 black and 3 red balls. If 3 balls are drawn at random what is the probability that all are black?
+ANS->3/11
+Q3.3 20 78 334 1696 ?
+ANS->(c)10206
+Q4.15.002 × ? × 25.0210 = 7113.018
+ANS->(A)19
+Q5.The sum of four consecutive integers is 102. Find the product of the extremes:
+ANS->(c)648
+
+
+     
